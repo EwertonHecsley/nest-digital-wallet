d8c91c1db7ec6726b242d5ae3dbd61511c73e666
Ewerton Hecsley <ewerton.martinscomercial@gmail.com>
2025-10-20 12:17:29 -0300

    feat: create service to hash password

---
+++ src/accounts/infra/services/service.module.ts
@@ -0,0 +1,12 @@
+import { Module } from "@nestjs/common";
+import { PasswordHashGateway } from "src/accounts/core/domain/ports/PasswordHashGateway";
+import { PasswordHashService } from "./PasswordHash.service";
+
+@Module({
+    providers: [{
+        provide:PasswordHashGateway,
+        useClass:PasswordHashService
+    }],
+    exports: [PasswordHashGateway]
+})
+export class ServiceModule{}
