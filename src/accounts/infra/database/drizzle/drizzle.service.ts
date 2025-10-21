67a51dcb5ecb6b467509783e4f195a750ab01783
Ewerton Hecsley <ewerton.martinscomercial@gmail.com>
2025-10-21 16:06:32 -0300

    feat: installation Drizzle ORM

---
+++ src/accounts/infra/database/drizzle/drizzle.service.ts
@@ -0,0 +1,11 @@
+import { Injectable, OnModuleDestroy } from "@nestjs/common";
+import { db, pool } from "./drizzle.config";
+
+@Injectable()
+export class DrizzleService implements OnModuleDestroy{
+    public readonly db = db;
+
+    async onModuleDestroy() {
+        await pool.end();
+    }
+}
